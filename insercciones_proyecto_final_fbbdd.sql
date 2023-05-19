c407c34e19921bd8eba8f89e4f7ff467d4490f68
agp00118 <agp00118@red.ujaen.es>
2023-05-19 17:40:49 +0200

    Tables SQL and Inserts SQL done

---
+++ insercciones_proyecto_final_fbbdd.sql
@@ -0,0 +1,19 @@
+INSERT INTO Estaciones VALUES (28001, 'Madrid', 'Miguel de Cervantes');
+INSERT INTO Estaciones VALUES (08001, 'Barcelona', 'Antoni Gaudí');
+INSERT INTO Estaciones VALUES (41001, 'Sevilla', 'Cristóbal Colón');
+INSERT INTO Estaciones VALUES (46001, 'Valencia', 'Sorolla');
+INSERT INTO Estaciones VALUES (48001, 'Bilbao', 'Pablo Picasso');
+INSERT INTO Estaciones VALUES (15001, 'A Coruña', 'Emilia Pardo Bazán');
+INSERT INTO Estaciones VALUES (29001, 'Málaga', 'Pablo Ruiz Picasso');
+INSERT INTO Estaciones VALUES (03001, 'Alicante', 'Óscar Esplá');
+INSERT INTO Estaciones VALUES (50001, 'Zaragoza', 'Francisco de Goya');
+INSERT INTO Estaciones VALUES (07001, 'Castellón', 'Ramón Llull');
+
+SELECT *
+    FROM Estaciones;
+    
+INSERT INTO Itinerarios VALUES (1, 'Madrid', 'Barcelona');
+INSERT INTO Itinerarios VALUES (2, 'Sevilla', 'Valencia');
+INSERT INTO Itinerarios VALUES (3, 'Bilbao', 'A Coruña');
+INSERT INTO Itinerarios VALUES (4, 'Málaga', 'Alicante');
+INSERT INTO Itinerarios VALUES (5, 'Zaragoza', 'Castellón');
